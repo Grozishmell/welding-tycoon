43b036dd69a856bac0e7d67c1e698705ab8ac3d6
Grozishmell <grozamarshmellow@gmail.com>
2026-08-13 14:55:38 +0300

    add interactable script

--- scripts/interaction/interactable.gd
+++ scripts/interaction/interactable.gd
@@ -1,2 +1,17 @@
 extends StaticBody3D
 class_name Interactable
+
+signal interacted(player: Player)
+
+@export var prompt_text: String = "Взаимодействовать"
+@export var enabled: bool = true
+
+
+func is_available() -> bool:
+	return enabled
+	
+
+func interact(player: Player) -> void:
+	if not is_available():
+		return
+	interacted.emit(player)
